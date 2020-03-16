1935794db8e42e98f8e0248383373b327102f6ce
cepheid42 <andrew.sexton.42@gmail.com>
2020-03-16 19:01:00 -0400

    making progress, but non-functional

--- gauss_mpi.c
+++ gauss_mpi.c
@@ -14,9 +14,7 @@ int *swap;
 int proc_id, nprocs;
 int NSIZE = 128;
 
-/* Timers for stuff */
-struct timeval total_start;
-struct timeval total_finish;
+MPI_Status status;
 
 /* Allocate the needed arrays */
 void allocate_memory() {
@@ -43,7 +41,7 @@ void allocate_memory() {
     assert(swap != NULL);
 }
 
-/* Initialize the matrix, but multithreaded! */
+/* Initialize the matrix */
 void initMatrix() {
     int i,j;
     for(i = 0 ; i < NSIZE; i++){
@@ -110,27 +108,27 @@ void getPivot(int currow) {
 
 
 
-/* Driver function, spawns threads for each row in matrix */
-
-void computeGauss(int begin, int end) {
-    int i,j,k;
-    double pivotVal;
-
-    for(i = begin; i < end; i++){
-        getPivot(i);
-
-        pivotVal = matrix[i][i];
-
-        for (j = i + 1 ; j < NSIZE; j++){
-            pivotVal = matrix[j][i];
-            matrix[j][i] = 0.0;
-            for (k = i + 1 ; k < NSIZE; k++){
-                matrix[j][k] -= pivotVal * matrix[i][k];
-            }
-            B[j] -= pivotVal * B[i];
-        }
-    }
-}
+///* Driver function, spawns threads for each row in matrix */
+//
+//void computeGauss(int begin, int end) {
+//    int i,j,k;
+//    int J;
+//    double pivotVal;
+//
+//    for(i = begin; i < end; i++){
+//        getPivot(i);
+//        MPI_Bcast(&J, 1, MPI_INT, )
+//        pivotVal = matrix[i][i];
+//        for (j = J + 1 + proc_id ; j < NSIZE; j += nprocs){
+//            pivotVal = matrix[j][J];
+//            matrix[j][J] = 0.0;
+//            for (k = i + 1 ; k < NSIZE; k++){
+//                matrix[j][k] -= pivotVal * matrix[i][k];
+//            }
+//            B[j] -= pivotVal * B[i];
+//        }
+//    }
+//}
 
 
 /* Linear Solver */
@@ -152,26 +150,27 @@ void solveGauss() {
 
 
 /* Print Times and such */
-void calc_time() {
+double calc_time(struct timeval *total_start, struct timeval *total_finish) {
     long compTime;
     double the_time;
 
-    compTime = (total_finish.tv_sec - total_start.tv_sec) * 1000000;
-    compTime = compTime + (total_finish.tv_usec - total_start.tv_usec);
+    compTime = (total_finish->tv_sec - total_start->tv_sec) * 1000000;
+    compTime = compTime + (total_finish->tv_usec - total_start->tv_usec);
     the_time = (double) compTime;
-    printf("MPI time: %f Secs\n", (double) the_time / 1000000.0);
+    return ((double) the_time / 1000000.0);
 }
 
 int main(int argc,char *argv[]) {
+    /* Init MPI things */
+    MPI_Init(&argc, &argv);
+
     int q;
     int verify = 0;
-    int begin, end;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
-
-    /* Modified to accept number of threads as input */
+    struct timeval total_start;
+    struct timeval total_finish;
+    double time;
+    /* Parse arguments */
     while((q = getopt(argc, argv, "vs:")) != -1){
         switch(q){
             case 's':
@@ -196,34 +195,106 @@ int main(int argc,char *argv[]) {
         }
     }
 
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+
+    printf("rank: %d out of %d\n", proc_id, nprocs);
     allocate_memory(NSIZE);
+    int from[nprocs];
+    /* Only root populates matrices */
+    if(proc_id == 0) {
+        initMatrix();
+    }
 
-    begin = (NSIZE*proc_id)/nprocs + 1;
-    end   = (NSIZE*(proc_id + 1))/nprocs;
-
-    initMatrix();
+    /* Synchronize all MPI things */
     MPI_Barrier(MPI_COMM_WORLD);
-    gettimeofday(&total_start, NULL);
+    if(proc_id == 1) {
+        gettimeofday(&total_start, NULL);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
 
     /* mpi solver */
-    computeGauss(begin, end);
+    double pivotVal;
+    int piv_row = 0;
+    double m_row[NSIZE];
 
+    if(proc_id == 0) {
+        printf("proc%d\n", proc_id);
+        for(int i = 0; i < NSIZE; i++){
+            // Create array of slice indices
+            for(int a = 0; a < nprocs; a++){
+                int slice = ((a * NSIZE) / (nprocs - 1)) + i + 1;
+                if(slice > (NSIZE - 1)) {
+                    slice = NSIZE - 1;
+                }
+                from[a] = slice;
+            }
+            getPivot(i);
+            piv_row = i;
+            // Give everyone updated copy of matrix
+
+
+            /* Broadcast is a send and receive
+             * so it cannot be inside the if statement
+             *
+             * so I need to redo this whole section
+            */
+            MPI_Bcast(matrix, NSIZE*NSIZE, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//            // Give everyone pivot row
+//            MPI_Bcast(&piv_row, 1, MPI_INT, 0, MPI_COMM_WORLD);
+//            // Give everyone new array slices
+//            MPI_Bcast(&from, nprocs, MPI_INT, 0, MPI_COMM_WORLD);
+//            // receive updated rows from helpers
+//            for(int p = 1; p < nprocs; p++) {
+//                for (int r = from[p]; r < from[p+1]; r++) {
+//                    MPI_Recv(&matrix[r], NSIZE, MPI_DOUBLE, p, p, MPI_COMM_WORLD, &status);
+//                }
+//            }
+        }
+    }
+    else {
+        printf("proc%d running\n", proc_id);
+//        int start = from[proc_id];
+//        int end = from[proc_id + 1];
+//
+//        // for each row in slice
+//        for(int r = start; r < end; r++){
+//            // From pivot (assume square matrix) to end of row
+//            for (int j = start; j < piv_row; j++){
+//                pivotVal = matrix[r][j];
+//                matrix[r][j] = 0.0;
+//                for(int k = j + 1; k < NSIZE; k++) {
+//                    matrix[j][k] -= (pivotVal * matrix[r][k]);
+//                }
+//            }
+//            MPI_Send(&matrix[r], NSIZE, MPI_DOUBLE, 0, proc_id, MPI_COMM_WORLD);
+//        }
+    }
+
+
+    /* Synchronize and get time */
     MPI_Barrier(MPI_COMM_WORLD);
-    gettimeofday(&total_finish, NULL);
-    calc_time();
+    if(proc_id == 1) {
+        gettimeofday(&total_finish, NULL);
+        time = calc_time(&total_start, &total_finish);
+        MPI_Send(&time, 1, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD);
+    }
+    if(proc_id == 0) {
+        MPI_Recv(&time, 1, MPI_DOUBLE, 1, 100, MPI_COMM_WORLD, &status);
+        printf("MPI time: %f\n", time);
+        /* Linear Solver for Verification */
+        if(verify) {
+            gettimeofday(&total_start, 0);
+            solveGauss();
+            gettimeofday(&total_finish, 0);
+            long compTime;
+            double the_time;
 
-    /* Linear Solver for Verification */
-    if(verify) {
-        gettimeofday(&total_start, 0);
-        solveGauss();
-        gettimeofday(&total_finish, 0);
-        long compTime;
-        double the_time;
-
-        compTime = (total_finish.tv_sec - total_start.tv_sec) * 1000000;
-        compTime = compTime + (total_finish.tv_usec - total_start.tv_usec);
-        the_time = (double) compTime;
-        printf("Verification time: %f Secs\n", (double) the_time / 1000000.0);
+            compTime = (total_finish.tv_sec - total_start.tv_sec) * 1000000;
+            compTime = compTime + (total_finish.tv_usec - total_start.tv_usec);
+            the_time = (double) compTime;
+            printf("Verification time: %f Secs\n", (double) the_time / 1000000.0);
+        }
     }
 
     MPI_Finalize();
